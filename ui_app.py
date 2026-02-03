764105ac57a1e5e656ec21142ec8d85965afecdc
yuuu454 <carlorossid@gmail.com>
2026-02-03 16:44:09 +0800

    Update ui_app.py

--- ui_app.py
+++ ui_app.py
@@ -1,7 +1,8 @@
 import os
 import json
 import streamlit as st
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_ollama import OllamaLLM
