76c0f8b2dfe0dd1c7971b41b27103119e262beb4
yuuu454 <carlorossid@gmail.com>
2026-02-03 17:01:31 +0800

    Update ui_app.py

--- ui_app.py
+++ ui_app.py
@@ -5,7 +5,9 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_ollama import OllamaLLM
+from langchain_community.llms import HuggingFaceHub
+import os
+
 import time
 import re  # <- anti-prompt-injection
 
