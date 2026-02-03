38b69de4f58990e5f059f3999545b32c935f9391
yuuu454 <carlorossid@gmail.com>
2026-02-03 17:46:40 +0800

    Update ui_app.py

--- ui_app.py
+++ ui_app.py
@@ -2,7 +2,7 @@ import os
 import json
 import streamlit as st
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-
+from langchain_community.llms import HuggingFaceEndpoint
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.llms import HuggingFaceHub
