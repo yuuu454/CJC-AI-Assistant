46ae4724151a85dd3bb0deceb33f8d251ea885e9
yuuu454 <carlorossid@gmail.com>
2026-02-08 16:19:16 +0800

    Update ui_app.py

--- ui_app.py
+++ ui_app.py
@@ -365,7 +365,7 @@ import streamlit as st
 def init_llm():
     return OllamaLLM(
         model="CFAIA:latest",
-        base_url="http://https://nonfamilial-alma-rubblier.ngrok-free.dev"
+        base_url="http://127.0.0.1:11434"
     )
 
 
