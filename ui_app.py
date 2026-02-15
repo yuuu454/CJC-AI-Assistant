749829d6619c5b31c6846924735f6f929b8ff7ab
yuuu454 <carlorossid@gmail.com>
2026-02-15 17:58:34 +0800

    Update ui_app.py

--- ui_app.py
+++ ui_app.py
@@ -359,7 +359,7 @@ def build_or_load_vector_store(text):
 def init_llm():
     return OllamaLLM(
         model="CFAIA:latest",
-        base_url="https://proud-liability-medication-mailed.trycloudflare.com"
+        base_url="https://transit-consultants-senior-emily.trycloudflare.com  "
       
 
     )
