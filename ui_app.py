98f49b9f46d8916506fd7a132343eb8e6a79bedb
yuuu454 <carlorossid@gmail.com>
2026-03-19 17:27:53 +0800

    Update ui_app.py

--- ui_app.py
+++ ui_app.py
@@ -354,7 +354,7 @@ def build_or_load_vector_store(text):
 def init_llm():
     return OllamaLLM(
         model="CFAIA:latest",
-        base_url="https://remember-nick-mono-england.trycloudflare.com"
+        base_url="https://struct-affordable-villas-phrase.trycloudflare.com"
       
 
     )
