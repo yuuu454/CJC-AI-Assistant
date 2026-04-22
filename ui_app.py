0053be2646079b805d68358ea2448b799c9fede6
yuuu454 <carlorossid@gmail.com>
2026-04-22 20:25:26 +0800

    Update ui_app.py

--- ui_app.py
+++ ui_app.py
@@ -475,7 +475,7 @@ def build_or_load_vector_store(text):
 def init_llm():
     return OllamaLLM(
         model="CFAIA:latest",
-        base_url="https://assessment-economies-occupied-involve.trycloudflare.com"
+        base_url="https://authorization-enables-involve-second.trycloudflare.com"
       
 
     )
